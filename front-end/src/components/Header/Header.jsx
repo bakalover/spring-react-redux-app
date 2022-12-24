8ad7459bd2e8615567c6e521688b3b52760306e9
VladTrofimchenko <trofimchenko.vladtrof@yandex.ru>
2022-12-25 00:11:04 +0300

    добавил инициализацию и регистрацию, урлы, пока не доделан граф, значения, и не очень разобрался с регистрацией

---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CSSModules from 'react-css-modules';
+import styles from './Header.module.css';
+import Logout from './Logout/Logout';
+
+const Header = (props) => {
+    const handleLogout = (e) => {
+      e.preventDefault();
+      props.logout();
+    }
+  
+    return (
+      <header styleName="main-header" className="theme">
+        {props.loggedUser && <Logout action={handleLogout} />}
+        <ul styleName="main-header__content info">
+          <li styleName="info__item">Лабораторная работа №4</li>
+          <li styleName="info__item">Вариант: 910008</li>
+        </ul>
+        <ul styleName="main-header__content authors">
+          <li styleName="authors__item">Константин Шпренгер (P32091)</li>
+          <li styleName="authors__item">Трофимченко Владислав (P32111)</li>
+        </ul>
+      </header>
+    );
+  }
+  
+  export default CSSModules(Header, styles, { allowMultiple: true, handleNotFoundStyleName: 'ignore' });
+ 
