ef0f6cccd2165e1013e758f67ca256c716826d7c
bakalover <105585491+bakalover@users.noreply.github.com>
2023-01-15 23:44:38 +0700

    Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,9 +5,17 @@ import AppContainer from 'components/AppContainer';
 import store from 'redux/store';
 import 'styles/style.css';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>,
+const root = ReactDOM.createRoot(
   document.getElementById('root')
-);
+  );
+
+  root.render(<Provider store={store}>
+         <AppContainer />
+       </Provider>)
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <AppContainer />
+//   </Provider>,
+//   document.getElementById('root')
+// );
