9b9d8965ebc66f9dddcc0f915f40e16b4950b651
bakalover <105585491+bakalover@users.noreply.github.com>
2023-01-15 23:34:10 +0700

    Update entryAPI.js

--- front-end/src/api/entryAPI.js
+++ front-end/src/api/entryAPI.js
@@ -1,33 +1,48 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/lab4/api/entries/'
+  //baseURL: 'http://localhost:3000/lab4/api/entries/'
+  baseURL: 'http://localhost:8080/api/shots/'
 });
 
 const entryAPI = {
   async getEntries(token) {
-    return axiosInstance.get('', {
+    return axiosInstance.get('all', {
       headers: {
-        Authorization: 'Bearer ' + token
+        Authorization: 'token:' + token
+        //Authorization: 'token:' + token
       }
     });
   },
 
-  async checkEntry(x, y, r, token) {
-    return axiosInstance.post('', { x, y, r }, {
-      headers: {
-        Authorization: 'Bearer ' + token
+  async checkEntry(x, y, r, token, status) {
+    for (let i = 0; i < x.length; i++) {
+      const elementX = x[i];
+      for (let j = 0; j < r.length; j++) {
+        const elementR = r[j];
+        axiosInstance.post('', {x, y, r, token, status}, {
+          headers: {
+            //Authorization: 'Bearer' + token
+            Authorization: 'token:' + token
+          }
+        });
       }
-    });
+
+    }
+    // return axiosInstance.post('', {x, y, r, token, status}, {
+    //   headers: {
+    //     Authorization: 'Bearer' + token
+    //   }
+    // });
   },
 
-  async clearEntries(token) {
-    return axiosInstance.delete('', {
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    });
-  }
+  // async clearEntries(token) {
+  //   return axiosInstance.delete('', {
+  //     headers: {
+  //       Authorization: 'Bearer' + token
+  //     }
+  //   });
+  // }
 }
 
 export default entryAPI;
