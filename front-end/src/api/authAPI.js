3469519d053c1726cc8706612f166c05791899b0
bakalover <105585491+bakalover@users.noreply.github.com>
2023-01-15 23:33:46 +0700

    Update authAPI.js

--- front-end/src/api/authAPI.js
+++ front-end/src/api/authAPI.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/lab4/api/auth/'
+  //baseURL: 'http://localhost:3000/lab4/api/auth/'
+  baseURL: 'http://localhost:8080/auth/'
 });
 
 const authAPI = {
   async login(username, password) {
-    return axiosInstance.post('login', { username, password });
+    //return axiosInstance.post('login', { username, password });
+    return axiosInstance.post('login', { username:username, password:password });
   },
 
   async register(username, password) {
-    return axiosInstance.post('register', { username, password });
+    //return axiosInstance.post('registration', { username, password });
+    return axiosInstance.post('registration', { username:username, password:password });
   },
 }
 
